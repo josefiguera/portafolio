79e7982dcacc53389480afe895ab156ded524769
josefiguera <52141975+josefiguera@users.noreply.github.com>
2019-06-23 20:00:56 -0500

    Create send.php

---
+++ send.php
@@ -0,0 +1,40 @@
+<?php
+if(isset($_POST['submit'])) 
+{
+$message=
+'Full Name:	'.$_POST['fullname'].'<br />
+Subject:	'.$_POST['subject'].'<br />
+Phone:	'.$_POST['phone'].'<br />
+Email:	'.$_POST['emailid'].'<br />
+Comments:	'.$_POST['comments'].'
+';
+    require "PHPMailer-master/class.phpmailer.php"; //include phpmailer class
+      
+    // Instantiate Class  
+    $mail = new PHPMailer();  
+      
+    // Set up SMTP  
+    $mail->IsSMTP();                // Sets up a SMTP connection  
+    $mail->SMTPAuth = true;         // Connection with the SMTP does require authorization    
+    $mail->SMTPSecure = "ssl";      // Connect using a TLS connection  
+    $mail->Host = "smtp.gmail.com";  //Gmail SMTP server address
+    $mail->Port = 465;  //Gmail SMTP port
+    $mail->Encoding = '7bit';
+    
+    // Authentication  
+    $mail->Username   = "josefiguera.portafolio@gmail.com"; // Your full Gmail address
+    $mail->Password   = "josemiel"; // Your Gmail password
+      
+    // Compose
+    $mail->SetFrom($_POST['emailid'], $_POST['fullname']);
+    $mail->AddReplyTo($_POST['emailid'], $_POST['fullname']);
+    $mail->Subject = "New Contact Form Enquiry";      // Subject (which isn't required)  
+    $mail->MsgHTML($message);
+ 
+    // Send To  
+    $mail->AddAddress("josefigueraburiel@gmail.com", "Jose Figuera"); // Where to send it - Recipient
+    $result = $mail->Send();		// Send!  
+	$message = $result ? '<div class="alert alert-success" role="alert"><strong>Success!</strong>Message Sent Successfully!</div>' : '<div class="alert alert-danger" role="alert"><strong>Error!</strong>There was a problem delivering the message.</div>';  
+	unset($mail);
+}
+?>
